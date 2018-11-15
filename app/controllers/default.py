5affc77cd5694cf34c1b5ee68808c25e42d64ed6
ThuBitencourtt <thubittencourt@gmail.com>
2018-11-15 21:52:09 -0200

    MVC02

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,29 @@
-from app import app 
+from flask import render_template
+from app import app, db 
 
+from app.models.tablers import tablers
+from app.models.forms import LoginForm
+
+@app.route("/index")
 @app.route("/")
 def index():
-	return "Hello World"
+	return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		print(form.username.data)
+		print(form.password.data)
+	else:
+		print(form.errors)
+	return render_template('login.html',
+							form=form)
+
+
+@app.route("/teste/<info>")
+@app.route("/teste", defaults=("info":None))
+def teste():						
+	i = User("thuani", "1234", "Thuani Bitencourtt", "thubittencourt@gmail.com")
+	db.session.add(i)
+	db.session.commit()
