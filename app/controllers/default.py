6c6e5ae90f4c109f5949d99bd2be99d57404cbb3
ThuBitencourtt <thubittencourt@gmail.com>
2018-11-24 20:12:08 -0200

    login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,8 +1,13 @@
-from flask import render_template
-from app import app, db 
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user
+from app import app, db, lm 
 
-from app.models.tablers import tablers
+from app.models.tables import User
 from app.models.forms import LoginForm
+
+@lm.user_loader
+def load_user(id):
+	return User.query.filter_by(id=id).first()
 
 @app.route("/index")
 @app.route("/")
@@ -12,18 +17,22 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
 	form = LoginForm()
-	if form.validate_on_submit():
-		print(form.username.data)
-		print(form.password.data)
+	if form.validate_on_submit():									
+		user = User.query.filter_by(username=form.username.data).first()
+		if user and user.password == form.password.data:
+			login_user(user)
+			flash("Logged in.")
+			return redirect(url_for("index"))
+		else:
+			flash("Invalid login.")
 	else:
 		print(form.errors)
 	return render_template('login.html',
 							form=form)
 
+@app.route("/logout")
+def logout():
+	logout_user()
+	flash("Logged out. ")
+	return redirect(url_for("index"))
 
-@app.route("/teste/<info>")
-@app.route("/teste", defaults=("info":None))
-def teste():						
-	i = User("thuani", "1234", "Thuani Bitencourtt", "thubittencourt@gmail.com")
-	db.session.add(i)
-	db.session.commit()
